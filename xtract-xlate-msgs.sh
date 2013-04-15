e67812616c2dc9bc01a4d848c9eee3a64301bcdf
Frank Brehm <frank.brehm@profitbricks.com>
2013-04-15 18:23:18 +0200

    Added xtract-xlate-msgs.sh

---
+++ xtract-xlate-msgs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pot_file="py_pb_dbhandler1.pot"
+output_dir="po"
+pkg_version="0.4.2"
+pkg_name="profitbricks-python-dbhandler"
+src_dir="src/pb_dbhandler"
+
+cd $(dirname $0)
+
+xgettext --output="${pot_file}" \
+        --output-dir="${output_dir}" \
+        --language="Python" \
+        --join-existing \
+        --add-comments \
+        --keyword=_ \
+        --keyword=__ \
+        --force-po \
+        --indent \
+        --add-location \
+        --width=85 \
+        --sort-by-file \
+        --package-name="${pkg_name}" \
+        --package-version="${pkg_version}" \
+        --msgid-bugs-address=frank.brehm@profitbricks.com \
+        $(find "${src_dir}" -type f -name '*.py' | sort)
+
+sed -i -e 's/msgid[ 	][ 	]*"/msgid "/' -e 's/msgstr[ 	][ 	]*"/msgstr "/' "${output_dir}/${pot_file}"
+
+# vim: ts=4 et
